d98fae220d5a46535f27ba8a4a338ab39e528888
Ibrahim-Ragab <Abrahammoustafa@hotmail.co.uk>
2019-10-16 17:15:31 +0200

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header'
+import Footer from './Footer'
 import './App.css';
 
 // The function that toggles between themes
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// The function that toggles between themes
+function Footer() {
+  return (
+    <div className="footer">
+      Made With <span className="heart-span">&#10084;</span> Ibrahim Ragab
+    </div>
+  );
+}
+
+export default Footer;
